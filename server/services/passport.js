c0ac438c27f64a502c8ad7a13c15b0eb9b924de9
Diego Krefta <diego.krefta@gmail.com>
2018-07-13 14:43:45 -0300

    Conditional if have a record or not

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      user.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have a record profile
+          done(null, existingUser);
+        } else {
+          // we don't have record profile
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
