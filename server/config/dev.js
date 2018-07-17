a169211b085439360d91a678bb6574d0e3be3558
Diego Krefta <diego.krefta@gmail.com>
2018-07-17 09:31:02 -0300

    finished auth flow

---
+++ server/config/dev.js
@@ -0,0 +1,11 @@
+//dev.js don't commit this!!
+
+module.exports = {
+  googleClientID:"1074995937242-c4jo1njh76f37gedfgdi097tdrr52rhf.apps.googleusercontent.com",
+  googleClientSecret: "CTSXeSSkEwt4cWNYJz7JPMNa",
+  mongoURI: 'mongodb://dkrefta:sk8rock@ds137651.mlab.com:37651/emaily-dev-full-stack',
+  cookieKey: 'aehuaehueahuaehuaeheauaheueah'
+};
+
+
+
