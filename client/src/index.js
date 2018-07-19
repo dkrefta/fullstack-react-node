e6f0b5a84ecbbafcfb428c580397fd7a36f26ff9
Diego Krefta <diego.krefta@gmail.com>
2018-07-19 17:01:51 -0300

    React Router Adjust

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
 const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
-      <App/>
+    <App/>
   </Provider>,
   document.querySelector("#root")
 );
