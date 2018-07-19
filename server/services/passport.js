84396b99f82fc56a22b12992279472c4e3468154
Diego Krefta <diego.krefta@gmail.com>
2018-07-19 15:17:46 -0300

    Refactor Async Await

--- server/services/passport.js
+++ server/services/passport.js
@@ -22,18 +22,14 @@ passport.use(
       callbackURL: "/auth/google/callback",
       prxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      user.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //we already have a record profile
-          done(null, existingUser);
-        } else {
-          // we don't have record profile
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await user.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
